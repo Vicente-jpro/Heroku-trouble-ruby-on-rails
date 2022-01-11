e2d5e70ec1a6d1cca7dc3960fdabe3a1ebdd02f7
vicente-jpro <vicenteviciii@outlook.com>
2022-01-11 15:42:02 +0100

    Initial commit

---
+++ heroku-links.rb
@@ -0,0 +1,22 @@
+
+Deploying with Git
+https://devcenter.heroku.com/articles/git
+
+Getting Started on Heroku with Rails 6.x
+https://devcenter.heroku.com/articles/getting-started-with-rails6
+
+
+'EISDIR: illegal operation on a directory' error when running CLI commands
+#986
+https://github.com/heroku/cli/issues/986
+
+Specifying a Ruby Version
+https://devcenter.heroku.com/articles/ruby-versions#your-ruby-version-is-x-but-your-gemfile-specified-y
+
+Heroku push rejected submodule
+https://stackoverflow.com/questions/13362288/heroku-push-rejected-submodule-install-failed
+
+1 - heroku create
+2 - heroku keys:add
+3 - heroku rename app-name
+
