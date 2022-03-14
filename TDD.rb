3b4459435bcbc941f4462aea21069daa7bd7ab3a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-14 18:53:35 +0100

    Update TDD.rb

--- TDD.rb
+++ TDD.rb
@@ -4,3 +4,6 @@ https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#configu
 Database Cleaner
 https://github.com/DatabaseCleaner/database_cleaner-active_record
 https://github.com/DatabaseCleaner/database_cleaner
+
+How to properly do model testing with ActiveStorage in rails?
+https://stackoverflow.com/questions/47583883/how-to-properly-do-model-testing-with-activestorage-in-rails
