7d2fda542095970dcaae99e6b711080f42b19ad6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-14 17:01:34 +0100

    Create TDD.rb

---
+++ TDD.rb
@@ -0,0 +1,6 @@
+factory_bot_rails
+https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#configure-your-test-suite
+
+Database Cleaner
+https://github.com/DatabaseCleaner/database_cleaner-active_record
+https://github.com/DatabaseCleaner/database_cleaner
