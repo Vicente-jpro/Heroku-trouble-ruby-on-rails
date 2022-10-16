50584c6cc41086136fa4b24145f6f695eaf6ce89
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-16 22:40:08 +0100

    Create freelancer_web_sites.rb

---
+++ freelancer_web_sites.rb
@@ -0,0 +1,97 @@
+17◾️ JustRemote
+
+📍https://justremote.co/
+
+Discover Remote Jobs from all around the world. Give up the commute, work remotely and do what you love, daily, & from anywhere.
+
+16◾️ AngelList
+
+📍https://angel.co/
+
+Apply privately to "140000+ remote jobs" and startup jobs near you with one application.
+
+
+14◾️ Working Nomads
+
+📍https://www.workingnomads.com/jobs
+
+Remote jobs for digital working nomads. Start your telecommuting career and work remotely from home.
+
+13◾️ Remotive
+
+📍https://remotive.com/
+
+Find the best remote job, working as a developer, customer support rep, product, or sales professional.
+
+12◾️ oDeskwork Private Limited
+
+📍https://odeskwork.com/
+
+"Work at oDesk Work" oDesk Work is an online portal & that provides freelancer services both for project hunters and project providers.
+
+11◾️ FlexJobs
+
+📍https://www.flexjobs.com/
+
+The #1 job search site for hand-screened flexible and remote jobs (work-from-home jobs) since 2007.
+
+10◾️ Fiverr
+
+📍https://lnkd.in/dzE5weTg
+
+Fiverr connects businesses with freelancers offering digital services in 500+ categories.
+
+9◾️ Upwork
+
+📍https://www.upwork.com/
+
+Upwork connects businesses with independent professionals & agencies all over the world.
+
+8◾️ Dribble
+
+📍https://dribbble.com/remote-design-jobs
+
+Discover 1500plus Remote Jobs designs on Dribble.
+
+7◾️ Freelancer.com
+
+📍https://www.freelancer.in/
+
+Find & hire top freelancers, web developers & designers inexpensively. World's largest marketplace of 60m.
+
+6◾️ Indeed.com
+
+📍https://in.indeed.com/m/
+
+With Indeed, you can search for millions of jobs online to find the next step in your career.
+
+5◾️ Outsourcely
+
+📍https://www.outsourcely.com/
+
+Outsourcely connects startups and businesses with talented remote workers from around the world. (400000+ Remote Workers & 50000+ Companies).
+
+4v We Work Remotely
+
+📍https://weworkremotely.com/
+
+We Work Remotely is the largest remote work community in the world.
+
+3◾️ ProBlogger
+
+📍https://problogger.com/
+
+Discover 100+ Remote jobs on multiple fields.
+
+1◾️ Remote.co
+
+📍https://remote.co/
+
+Remote.co is a definitive remote work job board for online job seekers and companies hiring. Start your remote job search here!
+
+------------------------------
+
+🗣What would you add?
+
+Happy learning!🙌
+
