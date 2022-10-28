76765cc77893d2f0c760300d6e713611e90a9946
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-28 06:25:28 +0100

    Create docker.rb

---
+++ docker.rb
@@ -0,0 +1,3 @@
+As seguintes assinaturas não puderam ser verificadas porque a chave pública não está disponível: NO_PUBKEY 4EB27DB2A3B88B8B
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys chave-publica-que-falta
