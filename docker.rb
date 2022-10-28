a58ac0c26e0709d77349b6b4cdc2f227cf3694cc
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-10-28 06:49:51 +0100

    Update docker.rb

--- docker.rb
+++ docker.rb
@@ -1,3 +1,5 @@
 As seguintes assinaturas não puderam ser verificadas porque a chave pública não está disponível: NO_PUBKEY 4EB27DB2A3B88B8B
-
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys chave-publica-que-falta
+
+Uninstall Redis
+https://askubuntu.com/questions/971188/uninstall-redis-after-manual-installation
