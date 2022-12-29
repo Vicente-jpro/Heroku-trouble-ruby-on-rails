a0fb5d90cd8eff5e8be6320e45f378e9558fad65
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-29 10:49:37 +0100

    Create spring_boot_errors.java

---
+++ spring_boot_errors.java
@@ -0,0 +1,11 @@
+The src/resources/application.yml file, and how to fix 'Could not resolve placeholder ${salt}' type errors
+
+ @Value("${salt}")
+  private String salt;
+
+<dependency>
+    <groupId>org.yaml</groupId>
+    <artifactId>snakeyaml</artifactId>
+</dependency>
+
+############################################################################
