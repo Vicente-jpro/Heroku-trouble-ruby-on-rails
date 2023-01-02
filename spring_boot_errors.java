50f0cd7d280811a4d4e34572aef87d3cf17aee3c
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-02 01:04:31 +0100

    Update spring_boot_errors.java

--- spring_boot_errors.java
+++ spring_boot_errors.java
@@ -9,3 +9,8 @@ The src/resources/application.yml file, and how to fix 'Could not resolve placeh
 </dependency>
 
 ############################################################################
+
+Address already in use: JVM_Bind" error?
+ https://stackoverflow.com/questions/12737293/how-do-i-resolve-the-java-net-bindexception-address-already-in-use-jvm-bind
+
+###########################################################################
