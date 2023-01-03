e4635d6f3eae6292e258489d615c758ceb0e376a
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-03 17:59:10 +0100

    Update spring_boot_errors.java

--- spring_boot_errors.java
+++ spring_boot_errors.java
@@ -11,6 +11,15 @@ The src/resources/application.yml file, and how to fix 'Could not resolve placeh
 ############################################################################
 
 Address already in use: JVM_Bind" error?
+ 
+If you know what port the process is running you can type: lsof -i:<port>.
+
+For instance, lsof -i:8080, to list the process (pid) running on port 8080.
+
+Then kill the process with kill <pid>
+ 
  https://stackoverflow.com/questions/12737293/how-do-i-resolve-the-java-net-bindexception-address-already-in-use-jvm-bind
 
 ###########################################################################
+
+
