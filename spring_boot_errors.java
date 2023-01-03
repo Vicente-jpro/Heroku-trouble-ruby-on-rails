80f66c55acd36d270e790f6febd75fb412bce0a3
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-03 18:03:40 +0100

    Update spring_boot_errors.java

--- spring_boot_errors.java
+++ spring_boot_errors.java
@@ -21,5 +21,35 @@ Then kill the process with kill <pid>
  https://stackoverflow.com/questions/12737293/how-do-i-resolve-the-java-net-bindexception-address-already-in-use-jvm-bind
 
 ###########################################################################
+ H2 database connection
+application:
+  name: TEST ENVIROMENT
+  
+spring:
+  datasource:
+    url: jdbc:h2:mem:testdb
+    driver-class-name: org.h2.Driver
+    username: sa
+    password: password
+  
+  jpa:
+    database-platform: org.hibernate.dialect.H2Dialect
+  
+  h2:
+    console:
+      enabled: true
+      path: /h2-console
+      
+# Dependencies
+#    <dependency>
+#      <groupId>org.springframework.boot</groupId>
+#      <artifactId>spring-boot-starter-data-jpa</artifactId>
+#    </dependency>
+#    
+#    <dependency>
+#      <groupId>com.h2database</groupId>
+#      <artifactId>h2</artifactId>
+#    </dependency>
+###############################################################################
 
 
