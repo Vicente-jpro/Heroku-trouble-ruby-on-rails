03d754231e3584e617fae737b9467960ba324fba
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2025-06-09 21:12:47 +0100

    Update envirorment_var.rb

--- envirorment_var.rb
+++ envirorment_var.rb
@@ -1,1 +1,13 @@
+Edite the file.
 EDITOR="code --wait" bin/rails credentials:edit
+
+
+How to use variables:
+Rails.application.credentials.redis_url
+# => "redis://localhost:6379"
+
+Rails.application.credentials.some_api_key
+# => "abc123"
+
+Rails.application.credentials.dig(:aws, :access_key_id)
+# => "YOUR_KEY"
